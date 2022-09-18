13f141b961a9ef0e9b8e72d0db0150e666048c91
Elizabeh <eliz01_01@yahoo.com>
2022-09-18 05:07:19 +0200

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary:
+        valeurs = list(a_dictionary.values())
+        cles = list(a_dictionary.keys())
+        return cles[valeurs.index(max(valeurs))]
+    else:
+        return None
+ #The index() method returns the index of the specified element in the list
