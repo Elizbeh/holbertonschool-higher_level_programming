d49780bf62e92d857b8a2f2a668dfa498bd30f98
Elizabeh <eliz01_01@yahoo.com>
2022-09-23 15:55:46 +0200

    pycodestyle correction

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,9 +5,9 @@ A function to adding two integersi
 
 
 def add_integer(a, b=98):
-    
+
     """Take the arguments to add.
-    
+
     a: One of the numbers, default not defined.
     b: The other number, set to default 98.
 
